2745c0f840ddf22d98cd64ec22ff414c013c05ce
MrPinabutter <vitorafael.r.c@gmail.com>
2022-04-27 21:41:52 -0300

    feat: homepage created

--- mobile/src/global/styles/theme.ts
+++ mobile/src/global/styles/theme.ts
@@ -2,10 +2,13 @@ export default {
   colors: {
     primary: '#5636D3',
     primaryDarker: '#2A2BA9',
+    primaryMoreDarker: '#1A1A69',
     background: '#EEE',
     white: '#FFF',
     gray: '#888888',
-    text: '#3B3B3B'
+    text: '#3B3B3B',
+    grayOpacity: 'rgba(0, 0, 0, 0.6)',
+    whiteOpacity: 'rgba(255, 255, 255, 0.6)'
   },
   fonts: {
     regular: 'Poppins_400Regular',
