dd84c6a717b0892944de6bdbbe650d207765d0b8
MrPinabutter <vitorafael.r.c@gmail.com>
2022-05-03 02:31:11 -0300

    fetch recipes

--- mobile/src/components/CardVertical/index.tsx
+++ mobile/src/components/CardVertical/index.tsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import { Dimensions } from 'react-native';
-import styled from 'styled-components/native';
+import React from "react";
+import { Dimensions, TouchableOpacityProps } from "react-native";
+import styled from "styled-components/native";
 
-const CardVertical = () => {
+interface CardProps extends TouchableOpacityProps {
+  image_url: string;
+  name: string;
+}
+
+const CardVertical = ({ image_url, name, ...rest }: CardProps) => {
   return (
-    <Container activeOpacity={0.7}  >
-      <Image source={{uri: 'https://img.itdg.com.br/tdg/images/blog/uploads/2017/07/shutterstock_413580649.jpg'}} />
+    <Container activeOpacity={0.7} {...rest}>
+      <Image source={{ uri: image_url }} />
       <TitleContainer>
-        <Title>
-          Feijão a milanesa
-        </Title>
+        <Title>{name}</Title>
       </TitleContainer>
     </Container>
   );
-}
-
+};
 
 const Container = styled.TouchableOpacity`
-  width: ${Dimensions.get('window').width * 0.4}px;
+  width: ${Dimensions.get("window").width * 0.4}px;
   height: 215px;
   border-radius: 33px;
   justify-content: flex-end;
-  background-color: ${({theme}) => theme.colors.white};
+  background-color: ${({ theme }) => theme.colors.white};
   padding: 16px;
   position: relative;
   overflow: hidden;
-  margin-right: ${Dimensions.get('window').width * 0.05}px;
-`
+  margin-right: ${Dimensions.get("window").width * 0.05}px;
+`;
 
 const Image = styled.Image`
   position: absolute;
@@ -34,18 +36,18 @@ const Image = styled.Image`
   left: 0;
   bottom: 0;
   right: 0;
-`
+`;
 
 const TitleContainer = styled.View`
   width: 100%;
   padding: 11px 12px;
-  background-color: ${({theme}) => theme.colors.whiteOpacity};
+  background-color: ${({ theme }) => theme.colors.whiteOpacity};
   border-radius: 16px;
-`
+`;
 
 const Title = styled.Text`
   font-size: 12px;
-  font-family: ${({theme}) => theme.fonts.medium};
-`
+  font-family: ${({ theme }) => theme.fonts.medium};
+`;
 
 export default CardVertical;
